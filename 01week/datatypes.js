fc218712aacd43864734c844bec6e9d77c692d76
David Bille <d_bille@yahoo.com>
2019-08-12 20:38:49 -0500

    Javascript practice

---
+++ 01week/datatypes.js
@@ -0,0 +1,50 @@
+// Write a JavaScript program to display the current day and time.
+// Write a JavaScript program to convert a number to a string.
+// Write a JavaScript program to convert a string to the number.
+
+
+
+
+
+// Write a JavaScript program that takes in different datatypes and prints out whether they are a: Boolean, Null, Undefined, Number, Nan or String.
+let myVariable = [1, 2, 3, 4];
+function dataTypeID(data) {
+  return console.log(typeof data);
+}
+dataTypeID(myVariable);
+
+
+// Write a JavaScript program that adds 2 numbers together.
+// function sumOf(x, y) {
+//   return (x + y);
+// }
+// sumOf(8, 4);
+
+
+// Write a JavaScript program that runs only when 2 things are true.
+// function chores(payBills, cleanApt) {
+//   if (payBills && cleanApt) {
+//     return ('All chores are done!')
+//   } else return ('Chores are not done!')
+// }
+// chores(true, true);
+
+
+// Write a JavaScript program that runs when 1 of 2 things are true.
+// function chores(payBills, cleanApt) {
+//   if (payBills || cleanApt) {
+//     return ('Chores are partially, if not fully done!!')
+//   } else return ('Chores are not done!')
+// }
+// chores(true, true);
+
+
+// Write a JavaScript program that runs when both things are not true.
+// function chores(payBills, cleanApt) {
+//   if ((payBills == false) && (cleanApt == false)) {
+//     return ('Nothing is done! Great work!')
+//   } else return ('You are too productive!')
+// }
+// chores(false, false);
+
+
