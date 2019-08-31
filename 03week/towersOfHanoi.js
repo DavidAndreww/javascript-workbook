08ed506b5e3db326c41e42e34ee55c5b41938481
David Bille <d_bille@yahoo.com>
2019-08-31 15:58:27 -0500

    logic and things

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -1,3 +1,38 @@
+//                   GAME START
+// check to see that the game board is set up correctly
+// all 4 pieces are aligned largest to smallest in array A
+
+//                   MOVING PIECES
+// Only one piece may be moved at a time
+// Make sure there is a piece on the array to be moved
+
+//  -- if ((stacks.a.length > 0) || (stacks.b.length > 0) || (stacks.b.length >0))
+//    
+
+// Pass array value into towersOfHanoi to give input for the startStack value. This value will 
+// then pop the last element from that array and store as a new variable while determining
+// where to place it 
+
+//  -- var pieceHolder = startStack.pop()
+
+
+// Pushing the piece back to the end of the array:
+//    -- Last value in the stack that you are pushing to, must be greater than the value of the 
+//    -- piece that is to be pushed
+
+//       if (endStack[endStack.length -1] > pieceHolder)
+
+// Value of pieceHolder must reset back to nothing once it has been pushed, to prepare to pick
+// up and hold the next value input
+
+//                  CHECK FOR WIN
+// If the number of elements in array B or C are equal to 4, and ordered from highest number
+// to lowest, the game has been won
+
+//    if ((stacks.b.length === 4) || (stacks.c.length === 4)){
+//      return 'You win!';
+//    }
+
 'use strict';
 
 const assert = require('assert');
@@ -22,6 +57,7 @@ function printStacks() {
 function movePiece() {
   // Your code here
 
+
 }
 
 function isLegal() {
@@ -36,7 +72,6 @@ function checkForWin() {
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
-  
 
 }
 
