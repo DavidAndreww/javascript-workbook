d25f3d22c8ebadf7bd258e857692e463a65e650d
David Bille <d_bille@yahoo.com>
2019-09-11 23:28:52 -0500

    problem 1 in the works

--- 05week/OOP_preworkNumber1.js
+++ 05week/OOP_preworkNumber1.js
@@ -1,14 +1,26 @@
 // https://www.w3resource.com/javascript-exercises/javascript-object-exercises.php
 
 
-//1. Write a JavaScript program to list the properties of a JavaScript object. Go to the editor
+//1. Write a JavaScript program to list the properties of a JavaScript object.
+// var student = {
+// name : "David Rayy",
+// sclass : "VI",
+// rollno : 12 };
 
-var student = {
-name : "David Rayy",
-sclass : "VI",
-rollno : 12 };
+//  ------------------------------------------------HELP ME
+// let decision = function isObject(obj) {
+//   let type = typeof obj;
+//   return type === 'object';
+// }
 
-function listProperties(x) {
-  console.log(x);
-}
-listProperties(student)
+
+// function getKeys(obj) {
+//   let keys = [];
+//   if (decision(student)) {
+//     for (var property in obj) {
+//       keys.push(obj.key);
+//     };
+//     console.log(keys);
+//   }
+// }
+// getKeys();
