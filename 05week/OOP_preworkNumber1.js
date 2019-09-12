02003acd146af98ce6879b14cebabf7cba09fc37
David Bille <d_bille@yahoo.com>
2019-09-11 22:55:35 -0500

    started prework problems

---
+++ 05week/OOP_preworkNumber1.js
@@ -0,0 +1,14 @@
+// https://www.w3resource.com/javascript-exercises/javascript-object-exercises.php
+
+
+//1. Write a JavaScript program to list the properties of a JavaScript object. Go to the editor
+
+var student = {
+name : "David Rayy",
+sclass : "VI",
+rollno : 12 };
+
+function listProperties(x) {
+  console.log(x);
+}
+listProperties(student)
