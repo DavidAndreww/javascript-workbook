c9e8f433a84a02be9d090a57468a925c0cea2c2f
David Bille <d_bille@yahoo.com>
2019-09-25 18:32:29 -0500

    connecting to the DOM

--- 07week/fetchPractice/main.js
+++ 07week/fetchPractice/main.js
@@ -1,17 +1,19 @@
 let arrayOfPosts;
 let arrayOfComments;
 let arrayOfUsers;
+let arrayOf5Posts;
 
 // this function waits for the web page to be loaded, when it does it will run the code inside of it which happen to be getPosts()
 window.onload = function() {
   getPosts()
   getComments()
   getUsers()
+  get5Posts()
 }
 
 // this function is going to make a fetch request to the url inside it's parameter brackets (). Then it will turn the response (data it's getting back), saved here as res. The res.json will not be saved as posts and saved into the variable, arrayOfPosts
 const getPosts = () => {
-  fetch('http://jsonplaceholder.typicode.com/posts')
+  fetch('http://jsonplaceholder.typicode.com/posts/')
     .then(res => res.json())
     .then(posts => arrayOfPosts = posts)
 }
@@ -28,9 +30,19 @@ const getUsers = () => {
   .then(users => arrayOfUsers = users)
 }
 
+const get5Posts = () => {
+  fetch('http://jsonplaceholder.typicode.com/posts/5')
+  .then(res => res.json())
+  .then(fivePosts => arrayOf5Posts = fivePosts)
+}
+
 // these functions log the results in your browsers console
 const consolePosts = () => {
   console.log(arrayOfPosts)
+}
+
+const console5Posts = () => {
+  console.log(arrayOf5Posts)
 }
 
 const consoleComments = () => {
@@ -40,6 +52,7 @@ const consoleComments = () => {
 const consoleUsers = () => {
   console.log(arrayOfUsers)
 }
+
 
 // this function creates elements inside the all-posts ul, then appends text inside it with the posts that were returned in the request.
 const displayPost = () => {
@@ -61,5 +74,25 @@ const displayComments = () => {
     const commentText = document.createTextNode(`Index # ${index}: - Name:${comments.name} - Comments: ${comments.body}`)
     commentList.appendChild(commentText)
     allComments.append(commentList)
+  })
+}
+
+const displayUsers = () => {
+  const allUsers = document.getElementById('all-users')
+  arrayOfUsers.map((users, index) => {
+    const userList = document.createElement('li')
+    const userText = document.createTextNode(`Index: ${index} - Name: ${users.name}`)
+    userList.appendChild(userText)
+    allUsers.append(userList)
+  })
+}
+
+const display5Posts = () => {
+  const f5vePosts = document.getElementById('5-posts')
+  arrayOf5Posts.map((fivePosts, index) => {
+    const fiveList = document.createElement('li')
+    const fiveText = document.createTextNode(`#${index}, Title: ${fivePosts}:`)
+    fiveList.appendChild(fiveText)
+    f5vePosts.append(fiveList)
   })
 }
