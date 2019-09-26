2fdfc80f847ba7e5f218d8d6b3167b576a34297c
David Bille <d_bille@yahoo.com>
2019-09-25 22:19:09 -0500

    address book homework

--- 07week/addressBook/main.js
+++ 07week/addressBook/main.js
@@ -1,63 +1,55 @@
 'use strict'
-let testArray;
 let arrayOfUsers;
 
 window.onload = function() {
-  getUser1()
   getUsers()
 }
 
-const getUser1 = () => {
+const getUsers = () => {
   fetch('https://randomuser.me/api/?results=5')
   .then(res => res.json())
-  .then(user1 => testArray = user1)
+  .then(user1 => arrayOfUsers = user1)
+  console.log(typeof arrayOfUsers)
 }
 
 
-const logUser = () => {
-  console.log(testArray)
+// arrayofusers is not an ARRAY
+
+const displayUsers = () => {
+  const allUsers = document.getElementById('address-book')
+  arrayOfUsers.map((user1) => {
+    const userList = document.createElement('li')
+    const userText = document.createTextNode(`Name:${user1}`)
+    userList.appendChild(userText)
+    allUsers.append(userList)
+  })
+}
+
+const displayPost = () => {
+  const allPosts = document.getElementById('all-posts')
+  arrayOfPosts.map((post, index) => {
+    const li = document.createElement('li')
+    const text = document.createTextNode(`#${index}, Title: ${post.title}:  ${post.body}, by user: ${post.userId}`)
+    li.appendChild(text)
+    allPosts.append(li)
+  })
 }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const getUsers = () => {
-Promise.all([
-  fetch('https://randomuser.me/api/'),
-  fetch('https://randomuser.me/api/'),
-  fetch('https://randomuser.me/api/'),
-  fetch('https://randomuser.me/api/')
-])
-.then(res => res.json())
-.then(users => arrayOfUsers.push(users))
-}
-
-console.log(arrayOfUsers)
-
-
-// const displayUsers = () => {
-//   const allUsers = document.getElementById('address-book')
-//   arrayOfUsers.map((users, index) => {
-//     const userList = document.createElement('li')
-//     const userText = document.createTextNode(`Name:${users.name} <br> ${users.picture}`)
-//     userList.appendChild(userText)
-//     allPosts.append(userList)
-//   })
+// const getUsers = () => {
+// Promise.all([
+//   fetch('https://randomuser.me/api/'),
+//   fetch('https://randomuser.me/api/'),
+//   fetch('https://randomuser.me/api/'),
+//   fetch('https://randomuser.me/api/')
+// ])
+// .then(res => res.json())
+// .then(users => arrayOfUsers.push(users))
 // }
+
+// console.log(arrayOfUsers)
+
 
