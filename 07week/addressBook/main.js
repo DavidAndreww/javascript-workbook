0cd0ef11be634b131d82be7bc5564998fc2ec9b3
David Bille <d_bille@yahoo.com>
2019-09-26 21:53:25 -0500

    things are working

--- 07week/addressBook/main.js
+++ 07week/addressBook/main.js
@@ -17,21 +17,38 @@ const consoleUsers = () => {
   console.log(arrayOfUsers.results)
 }
 
-const displayUsers = () => {
-  const allUsers = document.getElementById('address-book')
-  // const userImage = document.getElementById('div')
-  const userButton = document.getElementById('button')
-  for (let i = 0; i < arrayOfUsers.results.length; i++){
-    const userList = document.createElement('li')
-    const userImg = document.createElement('img')
-    const userBtn = document.createElement('button')
-    const userField = document.createTextNode(`Name:${arrayOfUsers.results[i].name.first} ${arrayOfUsers.results[i].name.last}`)
-    userImg.src = arrayOfUsers.results[i].picture.large;
-    // userImage.append(userImg)
-    userList.appendChild(userField) 
-    allUsers.append(userList) 
-    userButton.append(userBtn)
-   }
-}
+// const displayUsers = () => {
+//   const allUsers = document.getElementById('address-book')
+//   const userImage = document.getElementById('div')
+//   const userButton = document.getElementById('button')
+//   for (let i = 0; i < arrayOfUsers.results.length; i++){
+//     const userList = document.createElement('div')
+//     const userImg = document.createElement('img')
+//     const userBtn = document.createElement('button')
+//     const userField = document.createTextNode(`Name:${arrayOfUsers.results[i].name.first} ${arrayOfUsers.results[i].name.last}`)
+//     userImg.src = arrayOfUsers.results[i].picture.large;
+//     userImage.append(userImg)
+//     userList.appendChild(userField) 
+//     allUsers.append(userList) 
+//     userButton.append(userBtn)
+//    }
+// }
 
+const displayUsers = () => {
+  const accessNameDiv = document.getElementById('name')
+  const accessImgDiv = document.getElementById('img')
+  const accessButtonId = document.getElementById('button')
+
+  for(let i = 0; i <arrayOfUsers.results.length; i++){
+    const createNameNode = document.createElement('p')
+    const createImageElem = document.createElement('img')
+    const createButtonElem = document.createElement('button')
+    const pushName = document.createTextNode(`${arrayOfUsers.results[i].name.first} ${arrayOfUsers.results[i].name.last}`)
+    accessNameDiv.append(createNameNode)
+    createNameNode.appendChild(pushName)
+    createImageElem.src = arrayOfUsers.results[i].picture.large;
+    accessImgDiv.append(createImageElem)
+    accessButtonId.append(createButtonElem)
+  }
+}
 
