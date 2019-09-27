06e7fc4806df72a7d90ab66753fb1b78c5cf07a2
David Bille <d_bille@yahoo.com>
2019-09-27 00:07:23 -0500

    making progress

--- 07week/addressBook/main.js
+++ 07week/addressBook/main.js
@@ -1,19 +1,51 @@
 'use strict'
 
-// // creates an array to store JSON file
-// let arrayOfUsers;
+// creates an array to store JSON file
+let arrayOfUsers;
 
-// // action to take when window loads - call the getUsers() function
-// window.onload = function () {
-//   getUsers()
-// }
+// action to take when window loads - call the getUsers() function
+window.onload = function () {
+  getUsers()
+  displayUsers() // why doesn't this load userlist without butto click?
+}
 
-// // defines a function that fetches API, stores as a JSON file, and then gives its value to arrayOfUsers
-// const getUsers = () => {
-//   fetch('https://randomuser.me/api/?results=5')
-//     .then(res => res.json())
-//     .then(user1 => arrayOfUsers = user1)
-// }
+// defines a function that fetches API, stores as a JSON file, and then gives its value to arrayOfUsers
+const getUsers = () => {
+  fetch('https://randomuser.me/api/?results=5')
+    .then(res => res.json())
+    .then(user1 => arrayOfUsers = user1)
+}
+
+// function that gets users
+const displayUsers = () => {
+  // define the div that holds the content
+  const parentDiv = document.getElementById('personInfo');
+  // for each index (user) in the array, create <p>, <img>, <button> and textNode
+  for (let i = 0; i < arrayOfUsers.results.length; i++) {
+    const name_Para = document.createElement('p')
+    const imgElem = document.createElement('img')
+    const buttonElem = document.createElement('button')
+    const nameTextNode = document.createTextNode(`${arrayOfUsers.results[i].name.first} ${arrayOfUsers.results[i].name.last}`)
+    // textNode is put inside of the <p>, and <img> src attribute is defined
+    name_Para.appendChild(nameTextNode)
+    imgElem.src = arrayOfUsers.results[i].picture.large;
+    // the <p>, <img>, <button> and textNode are put inside the dive
+    parentDiv.append(imgElem)
+    parentDiv.append(name_Para)
+    parentDiv.append(buttonElem)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -63,46 +95,8 @@
 
 
 
-// May need to use this syntax for proper alignment in DOM?
-
-// const displayUsers = () => {
-//   const allUsers = document.getElementById('address-book')
-//   const userImage = document.getElementById('div')
-//   const userButton = document.getElementById('button')
-//   for (let i = 0; i < arrayOfUsers.results.length; i++){
-//     const userList = document.createElement('div')
-//     const userImg = document.createElement('img')
-//     const userBtn = document.createElement('button')
-//     const userField = document.createTextNode(`Name:${arrayOfUsers.results[i].name.first} ${arrayOfUsers.results[i].name.last}`)
-//     userImg.src = arrayOfUsers.results[i].picture.large;
-//     userImage.append(userImg)
-//     userList.appendChild(userField) 
-//     allUsers.append(userList) 
-//     userButton.append(userBtn)
-//    }
-// }
 
 
-// -------------------Current Working Code------------------------
-const displayUsers = () => {
-  // define the div that holds the content
-  const parentDiv = document.getElementById('personInfo');
 
 
-  // for each index (user) in the array, create <p>, <img>, <button> and textNode
-  for (let i = 0; i < arrayOfUsers.results.length; i++) {
-    const name_Para = document.createElement('p')
-    const imgElem = document.createElement('img')
-    const buttonElem = document.createElement('button')
-    const nameTextNode = document.createTextNode(`${arrayOfUsers.results[i].name.first} ${arrayOfUsers.results[i].name.last}`)
 
-    // textNode is put inside of the <p>, and <img> src attribute is defined
-    name_Para.appendChild(nameTextNode)
-    imgElem.src = arrayOfUsers.results[i].picture.large;
-
-    // the <p>, <img>, <button> and textNode are put inside the dive
-    parentDiv.append(imgElem)
-    parentDiv.append(name_Para)
-    parentDiv.append(buttonElem)
-  }
-}
