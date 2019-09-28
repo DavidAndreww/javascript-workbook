dc8c458a23eabc53c516f1b0736e058b1af8dc1a
David Bille <d_bille@yahoo.com>
2019-09-27 20:21:10 -0500

    trying to style expanded information to show up properly

--- 07week/addressBook/main.js
+++ 07week/addressBook/main.js
@@ -4,7 +4,7 @@
 let arrayOfUsers;
 
 // action to take when window loads - call the getUsers() function
-window.onload = function () {
+window.onload = () => {
   getUsers()
   // displayUsers() // why doesn't this load userlist without butto click?
 }
@@ -19,16 +19,16 @@ const getUsers = () => {
 // function that gets users
 const displayUsers = () => {
   // define the divs that holds the content
-  // const parentDiv = document.getElementById('personInfo');
   const nameDiv = document.getElementById('name');
   const photoDiv = document.getElementById('photo');
   const buttonDiv = document.getElementById('button');
   // for each index (user) in the array, create <p>, <img>, <button> and textNode
   for (let i = 0; i < arrayOfUsers.results.length; i++) {
-    let name_Para = document.createElement('p')
-    let imgElem = document.createElement('img')
-    let buttonElem = document.createElement('button')
-    const nameTextNode = document.createTextNode(`${arrayOfUsers.results[i].name.first} ${arrayOfUsers.results[i].name.last}`)
+    // define the <p>, <img>, <button> and textNode
+    const name_Para = document.createElement('p')
+    const imgElem = document.createElement('img')
+    const buttonElem = document.createElement('button')
+    let nameTextNode = document.createTextNode(`${arrayOfUsers.results[i].name.first} ${arrayOfUsers.results[i].name.last}`)
     // textNode is put inside of the <p>, and <img> src attribute is defined
     name_Para.appendChild(nameTextNode)
     imgElem.src = arrayOfUsers.results[i].picture.large;
@@ -36,25 +36,40 @@ const displayUsers = () => {
     nameDiv.append(name_Para)
     photoDiv.append(imgElem)
     buttonDiv.append(buttonElem)
-
+    
+    
+    
     // Will this work?
     // buttonElem.addEventListener("click", expandInfo())
 
     // Or maybe this will work
-    document.getElementsByTagName("button").setAttribute("class", "expandClass")
-    document.getElementsByClassName("expandClass").addEventListener("click", expandInfo())
+    // document.getElementsByTagName("button").setAttribute("class", "expandClass")
+    // document.getElementsByClassName("expandClass").addEventListener("click", expandInfo())
   }
 }
 
 const expandInfo = () => {
-  const expandedDiv = document.getElementById('expandInfo');
-  for (let j = 0; j < arrayOfUsers.results.length; j++){
-    let expandedInfo = document.createElement('p')
-    let expandedTextNode = document.createTextNode(`${arrayOfUsers.results[j].location.city}, ${arrayOfUsers.results[j].location.state} <br> ${arrayOfUsers.results[j].email} <br> ${arrayOfUsers.results[j].dob}`)
+  // define dlement that holds expanded info
+  const expandedLocationDiv = document.getElementById('expandLocation');
+  const expandedEmailDiv = document.getElementById('expandEmail');
+  const expandedAgeDiv = document.getElementById('expandAge');
+  // for each item in array, create <p> and textNode
+  for (let j = 0; j < arrayOfUsers.results.length; j++) {
+    let expandedLocation = document.createElement('sp')
+    let expandedEmail = document.createElement('span')
+    let expandedAge = document.createElement('span')
+    // create textNodes
+    let expandedLocationNode = document.createTextNode(`Location:${arrayOfUsers.results[j].location.city}`)
+    let expandedEmailNode = document.createTextNode(`Email ${arrayOfUsers.results[j].email}`)
+    let expandedAgeNode = document.createTextNode(`Age ${arrayOfUsers.results[j].dob.age}`)
     // puts textNode inside of the <p>
-    expandedInfo.appendChild(expandedTextNode)
+    expandedLocation.appendChild(expandedLocationNode)
+    expandedEmail.appendChild(expandedEmailNode)
+    expandedAge.appendChild(expandedAgeNode)
     // puts <p> inside of expandInfo div
-    expandedDiv.append(expandedInfo)
+    expandedLocationDiv.append(expandedLocation)
+    expandedEmailDiv.append(expandedEmail)
+    expandedAgeDiv.append(expandedAge)
   }
 }
 
